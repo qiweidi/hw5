4f01a9aca0a385528ae30e15ffe44dfa30fd6764
Weidi Qi <55336568+qiweidi@users.noreply.github.com>
2019-10-11 12:09:13 -0700

    Update concentric-circles.js

--- concentric-circles.js
+++ concentric-circles.js
@@ -1,1 +1,12 @@
+var circles = 9;
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  for (var i = 10; i <= 360; i = i + 10 ) {
+    circle(200, 200, 360-i);
+  }
+}
 
